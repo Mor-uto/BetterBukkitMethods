8c0d4d01248ac283c89aab71cd14b4c98d32f351
Moruto <79018613+MorutosBox@users.noreply.github.com>
2023-04-18 16:46:21 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,4 @@
-package me.pluginname.pluginname;
-
-import org.bukkit.ChatColor;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public final class pluginname extends JavaPlugin {
-
-    public String ColorTranslate(String text) {
-        text = ChatColor.translateAlternateColorCodes('&', text);
-        return text;
-    }
+public String ColorTranslate(String text) {
+	text = ChatColor.translateAlternateColorCodes('&', text);
+	return text;
 }
