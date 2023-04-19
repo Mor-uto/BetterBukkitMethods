94528397b8250158ef52ec5f3b093200cf3ed1d4
Moruto <79018613+MorutosBox@users.noreply.github.com>
2023-04-19 19:30:47 +0200

    Create ItemStackMaker.java

---
+++ ItemStackMaker.java
@@ -0,0 +1,15 @@
+//This is Meant To Make Items Faster For GUIS So U Do
+//ItemStack item1 = new createItemStackGUI(Material.STONE, "test name");
+//instead of making a new itemstack then itemmeta then doing displayname
+//then setting the item's meta, this is soo much better and way faster
+  
+public ItemStack createItemStackGUI(Material type, String DisplayName) {
+	ItemStack item = new ItemStack(type, 1);
+	ItemMeta meta = item.getItemMeta();
+  
+	meta.setDisplayName(plugin.ColorTranslate(DisplayName));
+  
+	item.setItemMeta(meta);
+  
+	return item;
+}
