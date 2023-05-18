bcf402758d285abd67316458774f9bdf2bcdf501
Moruto <79018613+MorutosBox@users.noreply.github.com>
2023-05-19 00:05:27 +0300

    Update ColorTranslator(Shortened).java

--- ColorTranslator(Shortened).java
+++ ColorTranslator(Shortened).java
@@ -1,6 +1,5 @@
 //This Is Meant To Be A Shortner So You Dont Have Long Code For Each String
 
 public String ColorTranslate(String text) {
-	text = ChatColor.translateAlternateColorCodes('&', text);
-	return text;
+	return ChatColor.translateAlternateColorCodes('&', text);
 }
