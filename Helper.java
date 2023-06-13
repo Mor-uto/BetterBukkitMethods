63e69b206f774a4e0c213c391c29c2994fd6fb2d
Moruto <79018613+Mor-uto@users.noreply.github.com>
2023-06-13 14:11:20 +0300

    Update Helper.java

--- Helper.java
+++ Helper.java
@@ -1,3 +1,14 @@
 public void sendMessage(Player player, String message) {
 	player.SendMessage(ChatColor.translateAlternativeColorCodes('&', message));
 }
+
+public ItemStack createItemStackGUI(Material type, String DisplayName) {
+	ItemStack item = new ItemStack(type, 1);
+	ItemMeta meta = item.getItemMeta();
+  
+	if (DisplayName != null) meta.setDisplayName(plugin.ColorTranslate(DisplayName));
+  
+	item.setItemMeta(meta);
+  
+	return item;
+}
