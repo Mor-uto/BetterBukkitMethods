45c0a6a0145188db2c8a5f6b9c94cf585ddab720
Moruto <79018613+Mor-uto@users.noreply.github.com>
2023-06-16 06:10:14 +0300

    Update Helper.java

--- Helper.java
+++ Helper.java
@@ -1,14 +1,23 @@
-public void sendMessage(Player player, String message) {
-	player.SendMessage(ChatColor.translateAlternativeColorCodes('&', message));
-}
+//You will have to add in the imports and the package manually
 
-public ItemStack createItemStackGUI(Material type, String DisplayName) {
-	ItemStack item = new ItemStack(type, 1);
-	ItemMeta meta = item.getItemMeta();
-  
-	if (DisplayName != null) meta.setDisplayName(plugin.ColorTranslate(DisplayName));
-  
-	item.setItemMeta(meta);
-  
-	return item;
+public class Helper {
+
+	public void sendMessage(Player player, String message) {
+		player.SendMessage(ChatColor.translateAlternativeColorCodes('&', message));
+	}
+
+	public ItemStack createIS(Material type, String DisplayName) {
+		ItemStack item = new ItemStack(type, 1);
+		ItemMeta meta = item.getItemMeta();
+
+		if (DisplayName != null) meta.setDisplayName(plugin.ColorTranslate(DisplayName));
+
+		item.setItemMeta(meta);
+
+		return item;
+	}
+
+	public String transColor(String text) {
+		return ChatColor.translateAlternateColorCodes('&', text);
+	}
 }
